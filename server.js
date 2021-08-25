f0341e1105d28ca849816dc27483c444257535bc
Avinash <pargaveavinash@gmail.com>
2021-08-25 22:52:25 +0530

    auth user

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const connectDB = require('./config/db');
+const connectDB = require("./config/db");
 const { post } = require("request");
 
 const app = express();
@@ -7,13 +7,16 @@ const app = express();
 //Connect Database
 connectDB();
 
+// Init Middleware
+app.use(express.json({ extended: false }));
+
 app.get("/", (req, res) => res.send("API Running"));
 
 //Define Routes
-app.use('/api/users', require('./routes/api/users'));
-app.use('/api/auth', require('./routes/api/auth'));
-app.use('/api/profile', require('./routes/api/profile'));
-app.use('/api/posts', require('./routes/api/posts'));
+app.use("/api/users", require("./routes/api/users"));
+app.use("/api/auth", require("./routes/api/auth"));
+app.use("/api/profile", require("./routes/api/profile"));
+app.use("/api/posts", require("./routes/api/posts"));
 
 const PORT = process.env.PORT || 5000;
 
